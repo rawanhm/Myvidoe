2d791c15a85215a3a53a60aab8f9bbd11743e8f2
Rawan Almegren <Ralmegren@elm.sa>
2023-04-06 11:55:09 +0300

    security

--- src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -1,31 +1,27 @@
 package com.example.demo.security;
 
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-import java.util.Date;
-
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import com.auth0.jwt.JWT;
+import com.example.demo.model.persistence.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.naming.AuthenticationException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import com.auth0.jwt.JWT;
-import com.example.demo.model.persistence.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import sun.security.util.SecurityConstants;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private Logger log = LoggerFactory.getLogger(JWTAuthenticationFilter.class);
@@ -35,6 +31,35 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+        try {
+            User creds = new ObjectMapper().readValue(req.getInputStream(), User.class);
 
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getUsername(),
+                            creds.getPassword(),
+                            new ArrayList<>())
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+        String username = ((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername();
+        String token = JWT.create()
+                .withSubject(username)
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .sign(HMAC512(SecurityConstants.SECRET.getBytes()));
+        res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+        log.info("[LOGIN] [Success] for user : " + username);
+    }
 
 }
